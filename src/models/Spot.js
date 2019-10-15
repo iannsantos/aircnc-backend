dc3d085dab6df743d3cbf01f68ace4b63ea1e241
Iann Santos <iannsantos8@gmail.com>
2019-10-14 21:03:05 -0300

    little adjustments

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -11,6 +11,17 @@ const SpotSchema = new mongoose.Schema({
     // está se referindo ao model User
     ref: 'User'
   }
+},
+{
+  // toda vez que o shema for convertido em JSON, será calculado os virtuals
+  toJSON: {
+    virtuals: true
+  }
+});
+
+// o campo não existe no sistema, será computado pelo JS
+SpotSchema.virtual('thumbnail_url').get(function () {
+  return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 const mongodbUrl = require('./mongodbUrl');
 const routes = require('./routes');
 
@@ -17,6 +19,7 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use('/api', routes);
 
 app.listen(3333);
